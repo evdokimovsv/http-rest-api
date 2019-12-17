315a50b384967a1034c435ae0fc726de30526194
Conker <conker@localhost.localdomain>
2019-12-17 19:04:04 +0500

    Third stage

--- go.mod
+++ go.mod
@@ -4,8 +4,12 @@ go 1.13
 
 require (
 	github.com/BurntSushi/toml v0.3.1
+	github.com/asaskevich/govalidator v0.0.0-20190424111038-f61b66f89f4a // indirect
+	github.com/go-ozzo/ozzo-validation v3.6.0+incompatible
+	github.com/go-ozzo/ozzo-validation/v3 v3.8.1
 	github.com/gorilla/mux v1.7.3
 	github.com/lib/pq v1.2.0
 	github.com/sirupsen/logrus v1.4.2
 	github.com/stretchr/testify v1.4.0
+	golang.org/x/crypto v0.0.0-20191206172530-e9b2fee46413
 )
--- model/user.go
+++ model/user.go
@@ -1,8 +1,46 @@
 package model
 
+import (
+	validation "github.com/go-ozzo/ozzo-validation/v3"
+	"github.com/go-ozzo/ozzo-validation/v3/is"
+	"golang.org/x/crypto/bcrypt"
+)
+
 //User ...
 type User struct {
 	ID                int
 	Email             string
+	Password          string
 	EncryptedPassword string
+}
+
+// Validate ...
+func (u *User) Validate() error {
+	return validation.ValidateStruct(
+		u,
+		validation.Field(&u.Email, validation.Required, is.Email),
+		validation.Field(&u.Password, validation.By(requiredIf(u.EncryptedPassword == "")), validation.Length(6, 200)),
+	)
+}
+
+// BeforeCreate ...
+func (u *User) BeforeCreate() error {
+	if len(u.Password) > 0 {
+		enc, err := encryptString(u.Password)
+		if err != nil {
+			return err
+		}
+
+		u.EncryptedPassword = enc
+	}
+
+	return nil
+}
+
+func encryptString(s string) (string, error) {
+	b, err := bcrypt.GenerateFromPassword([]byte(s), bcrypt.MinCost)
+	if err != nil {
+		return "", err
+	}
+	return string(b), nil
 }
